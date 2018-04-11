c64f0d3a68b65c29b45341c6c81f1d7ac5a1048a
TheLox95 <leo_9_t@hotmail.com>
2018-04-11 15:20:28 -0400

    chart package added

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\Debugbar\ServiceProvider::class,
+        Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -210,6 +211,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Lava' => Khill\Lavacharts\Laravel\LavachartsFacade::class,
 
     ],
 
